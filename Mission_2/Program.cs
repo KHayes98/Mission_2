70924f6101fa10fff7b138c4fd151e6a3c12d3c4
Reboot <kchayes2016@gmail.com>
2023-01-19 02:30:45 -0700

    More progress

--- Mission_2/Program.cs
+++ Mission_2/Program.cs
@@ -19,6 +19,36 @@ namespace Mission_2
             Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
             Console.WriteLine("Each '*' represents 1% of the total number of rolls.");
             Console.WriteLine("Total number of rolls = " + diceRolls);
+
+            Random r = new Random();
+
+            int die_1 = 0;
+            int die_2 = 0;
+
+            int[] rollTotals = new int[diceRolls];
+
+            for (int i = 0; i < diceRolls; i++)
+            {
+                die_1 = r.Next(1, 7);
+                die_2 = r.Next(1, 7);
+
+                Console.WriteLine(i + ": " + die_1);
+                Console.WriteLine(i + ": " + die_2);
+
+                rollTotals[i] = die_1 + die_2;
+            }
+
+            for (int i = 0; i < diceRolls; i++)
+            {
+                Console.WriteLine(i + ": " + rollTotals[i]);
+            }
+
+            for (int i_outer = 2; i_outer < 13; i_outer++)
+            {
+                
+            }
+
+            
         }
     }
 }
