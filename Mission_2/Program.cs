4e25848e0172f774cf4ef32eec92297baae1108d
Reboot <kchayes2016@gmail.com>
2023-01-18 23:29:08 -0700

    Recent changes

--- Mission_2/Program.cs
+++ Mission_2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Mission_2
+{
+    public class Program
+    {
+        static void Main (string[] args)
+        {
+            string ?userInput = "";
+            int diceRolls = 0;
+
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+
+            Console.WriteLine("How many dice rolls would you like to simulate?");
+            userInput = Console.ReadLine();
+            //Converts to int
+            diceRolls= Convert.ToInt32(userInput);
+
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each '*' represents 1% of the total number of rolls.");
+            Console.WriteLine("Total number of rolls = " + diceRolls);
+        }
+    }
+}
